b51e5504d2ac7b809973f3850d3cb632724e8f7d
fan <291950304@qq.com>
2025-05-31 17:17:18 +0800

    feat: button component

---
+++ src/components/button/Button.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { cva } from 'class-variance-authority'
+import { cn } from '@/lib/utils'
+
+const buttonVariants = cva(
+  "font-head transition-all outline-hidden cursor-pointer duration-200 font-medium flex items-center",
+  {
+    variants: {
+      variant: {
+        default:
+          "shadow-md hover:shadow-none bg-primary text-black border-2 border-black transition hover:translate-y-1 hover:bg-primary-hover",
+        secondary:
+          "shadow-md hover:shadow-none bg-secondary shadow-primary text-secondary-foreground border-2 border-black transition hover:translate-y-1",
+        outline:
+          "shadow-md hover:shadow-none bg-transparent border-2 transition hover:translate-y-1",
+        link: "bg-transparent hover:underline",
+      },
+      size: {
+        sm: "px-3 py-1 text-sm shadow hover:shadow-none",
+        md: "px-4 py-1.5 text-base",
+        lg: "px-8 py-3 text-lg",
+        icon: "p-2",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+      variant: "default",
+    },
+  },
+);
+
+const props = defineProps<{
+  variant?: "default" | "secondary" | "outline" | "link";
+  size?: "sm" | "md" | "lg" | "icon";
+}>();
+</script>
+
+<template>
+  <button :class="cn(buttonVariants({ variant, size }), $attrs.class)">
+    <slot />
+  </button>
+</template>
