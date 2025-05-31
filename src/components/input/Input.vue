4fed24ee1fdc3b74c83da5547b7895b8f285435b
fan <291950304@qq.com>
2025-05-31 17:21:05 +0800

    fix: incorrect border color on search box

--- src/components/input/Input.vue
+++ src/components/input/Input.vue
@@ -6,24 +6,38 @@ interface InputProps extends /* @vue-ignore */ InputHTMLAttributes {
   class?: string
 }
 
-withDefaults(defineProps<InputProps>(), {
+const props = withDefaults(defineProps<InputProps>(), {
   type: 'text',
   placeholder: '请输入',
+  autocapitalize: 'off',
+  inputmode: 'text',
+  spellcheck: 'false',
+  autocorrect: 'off',
+  autocomplete: 'off',
 })
+
+const modelValue = defineModel()
 </script>
 
 <template>
   <div
-    :class="cn('flex px-4 py-2 items-center border-2 shadow-md transition focus-within:shadow-xs',
-      { 'border-red-500 text-red-500 shadow-xs shadow-red-600': $attrs['aria-invalid'] },
+    :class="cn('flex px-4 py-2 items-center border-2 border-secondary shadow-md transition focus-within:shadow-xs focus-within:border-secondary',
+      { 'border-destructive text-destructive shadow-xs': $attrs['aria-invalid'] },
       $props.class,
     )"
   >
     <slot name="prefix" />
     <input
-      :type="type"
-      :placeholder="placeholder"
-      class="flex-1 px-4 py-2 focus:outline-hidden"
+      :value="modelValue"
+      @input="modelValue = ($event.target as HTMLInputElement).value"
+      :type="props.type"
+      :placeholder="props.placeholder"
+      :autocapitalize="props.autocapitalize"
+      :inputmode="props.inputmode"
+      :spellcheck="props.spellcheck"
+      :autocorrect="props.autocorrect"
+      :autocomplete="props.autocomplete"
+      class="flex-1 px-2 text-base focus:outline-none bg-transparent text-foreground placeholder:text-muted-foreground"
       v-bind="$attrs"
     />
     <slot name="suffix" />
--- src/newtab/components/SearchBar.vue
+++ src/newtab/components/SearchBar.vue
@@ -26,59 +26,55 @@ const handleKeyPress = (e: KeyboardEvent) => {
 
 <template>
   <div class="w-full max-w-[600px] my-5 mx-auto">
-    <div>
-      <Input
-        v-model="searchQuery"
-        type="text"
-        placeholder="输入搜索内容"
-        class="rounded-xl border-[var(--border)]"
-        @keypress="handleKeyPress"
-      >
-        <template #prefix>
-          <Popover class="w-dvw max-w-[600px]" align="start" :alignOffset="-40" :sideOffset="40">
-            <template #trigger>
-              <img
-                :src="selectedEngine.icon"
-                class="w-6 h-6 rounded-full cursor-pointer"
-                alt="搜索引擎"
-              />
-            </template>
-            <div class="grid grid-cols-[repeat(auto-fill,minmax(80px,1fr))] gap-4 p-2">
-              <div
-                v-for="(engine, index) in searchEngines"
-                :key="engine.name"
-                :class="cn(' relative flex flex-col items-center justify-center aspect-square p-3 rounded-xl',
-                 'cursor-pointer transition-all duration-200 ease-in-out border-2 border-secondary hover:-translate-y-0.5',
-                  { 'border-2 border-primary': selectedEngine.name === engine.name }
-                )"
-                @click="selectEngine(engine)"
+    <Input
+      v-model="searchQuery"
+      placeholder="输入搜索内容"
+      class="rounded-xl py-4"
+      @keypress="handleKeyPress"
+    >
+      <template #prefix>
+        <Popover class="w-dvw max-w-[600px]" align="start" :alignOffset="-40" :sideOffset="40">
+          <template #trigger>
+            <img
+              :src="selectedEngine.icon"
+              class="w-8 h-8 px-1 py-1 rounded-full cursor-pointer border-2 border-border"
+              alt="搜索引擎"
+            />
+          </template>
+          <div class="grid grid-cols-[repeat(auto-fill,minmax(80px,1fr))] gap-4 p-2">
+            <div
+              v-for="(engine, index) in searchEngines"
+              :key="engine.name"
+              :class="cn(' relative flex flex-col items-center justify-center aspect-square p-3 rounded-xl',
+                'cursor-pointer transition-all duration-200 ease-in-out border-2 border-secondary hover:-translate-y-0.5',
+                { 'border-2 border-primary': selectedEngine.name === engine.name }
+              )"
+              @click="selectEngine(engine)"
+            >
+              <img class="w-5 h-5 mb-2" :src="engine.icon" :alt="engine.name" />
+              <span class="text-xs text-secondary text-center overflow-hidden text-ellipsis whitespace-nowrap w-full">{{ engine.name }}</span>
+              <button
+                class="absolute top-1 right-1 w-5 h-5 rounded-full cursor-pointer flex items-center justify-center"
+                @click.stop="removeSearchEngine(index)"
+                title="删除搜索引擎"
               >
-                <img class="w-5 h-5 mb-2" :src="engine.icon" :alt="engine.name" />
-                <span class="text-xs text-secondary text-center overflow-hidden text-ellipsis whitespace-nowrap w-full">{{ engine.name }}</span>
-                <button
-                  class="absolute top-1 right-1 w-5 h-5 rounded-full cursor-pointer flex items-center justify-center"
-                  @click.stop="removeSearchEngine(index)"
-                  title="删除搜索引擎"
-                >
-                  <Icon icon="material-symbols:close-small-outline-rounded" width="24" height="24" />
-                </button>
-              </div>
-              <!-- 添加按钮 -->
-              <div
-                class="flex flex-col items-center justify-center aspect-square p-3 rounded-xl cursor-pointer transition-all duration-200 ease-in-out border-2 border-secondary hover:-translate-y-0.5"
-                @click="openAddEngineDialog"
-              >
-                <Icon class="text-secondary" icon="material-symbols:add-2-rounded" width="24" height="24" />
-              </div>
+                <Icon icon="material-symbols:close-small-outline-rounded" width="24" height="24" />
+              </button>
             </div>
-          </Popover>
-        </template>
-        <template #suffix>
-          <button @click="handleSearch" class="cursor-pointer">
-            <Icon icon="material-symbols:search-rounded" width="24" height="24" />
-          </button>
-        </template>
-      </Input>
-    </div>
+            <div
+              class="flex flex-col items-center justify-center aspect-square p-3 rounded-xl cursor-pointer transition-all duration-200 ease-in-out border-2 border-secondary hover:-translate-y-0.5"
+              @click="openAddEngineDialog"
+            >
+              <Icon class="text-secondary" icon="material-symbols:add-2-rounded" width="24" height="24" />
+            </div>
+          </div>
+        </Popover>
+      </template>
+      <template #suffix>
+        <button @click="handleSearch" class="cursor-pointer">
+          <Icon icon="material-symbols:search-rounded" width="24" height="24" class="text-secondary" />
+        </button>
+      </template>
+    </Input>
   </div>
 </template>
