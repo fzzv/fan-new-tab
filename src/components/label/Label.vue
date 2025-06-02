e69bc67f6679c2d39213428f905640300f8934f0
fan <291950304@qq.com>
2025-06-02 17:36:49 +0800

    chore: label component

---
+++ src/components/label/Label.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Label } from 'reka-ui'
+import { cva } from 'class-variance-authority'
+import { cn } from '@/lib/utils'
+
+const labelVariants = cva(
+  'leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+)
+</script>
+
+<template>
+  <Label :class="cn(labelVariants())" v-bind="$attrs">
+    <slot />
+  </Label>
+</template>
