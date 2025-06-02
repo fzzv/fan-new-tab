e3edd8848cb16e8d93a76676a224ccf4bb2a3772
fan <291950304@qq.com>
2025-06-03 00:29:34 +0800

    chore: 书签功能

---
+++ src/newtab/components/Bookmarks.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Tabs } from '@/components/tabs'
+
+const tabs = [
+  {
+    value: 'bookmarks',
+    label: 'Bookmarks',
+    icon: 'material-symbols:bookmarks-outline-rounded',
+  },
+  {
+    value: 'bookmarks1',
+    label: '1b',
+    icon: 'material-symbols:bookmarks-outline-rounded',
+  },
+]
+
+const modelValue = ref('bookmarks')
+</script>
+
+<template>
+  <Tabs :tabs="tabs" v-model="modelValue">
+    <template #bookmarks>
+      {{ modelValue }}
+    </template>
+    <template #bookmarks1>
+      {{ modelValue }}
+    </template>
+  </Tabs>
+</template>
