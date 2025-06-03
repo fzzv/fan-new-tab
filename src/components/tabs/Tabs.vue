196aaec0970c1466d8c65fcb56ed7615c22eeeb9
fan <291950304@qq.com>
2025-06-03 17:10:14 +0800

    feat: 给tab添加右击菜单功能

--- src/components/tabs/Tabs.vue
+++ src/components/tabs/Tabs.vue
@@ -2,17 +2,36 @@
 import { TabsRoot, TabsList, TabsTrigger, TabsContent } from './index'
 import { Icon } from '@iconify/vue'
 import { cn } from '@/lib/utils'
+import { useContextMenu } from '@/composables/useContextMenu.ts'
+import ContextMenu from '@/components/context-menu/ContextMenu.vue'
+import type { Placement } from '@popperjs/core'
+import type { MenuItemType } from '@/types'
 
-defineProps<{
+interface TabItem {
+  label: string
+  value: string
+  icon?: string
+  activeIcon?: string
+}
+
+interface TabsProps {
   asChild?: boolean
-  tabs: {
-    value: string
-    label: string
-    icon: string
-  }[]
-}>()
+  tabs: TabItem[]
+  items?: MenuItemType[]
+  popperPlacement?: Placement
+  enableContextMenu?: boolean
+}
+
+withDefaults(defineProps<TabsProps>(), {
+  tabs: () => [],
+  popperPlacement: () => 'right-start',
+  enableContextMenu: () => true,
+})
 
 const modelValue = defineModel<string>('modelValue', { required: true })
+
+// 右击菜单
+const { isOpen, virtualElement, onContextMenu } = useContextMenu()
 </script>
 
 <template>
@@ -22,11 +41,16 @@ const modelValue = defineModel<string>('modelValue', { required: true })
         v-for="tab in tabs"
         :key="tab.value"
         :value="tab.value"
-        :class="
-          cn('flex items-center gap-2 px-4 py-1 border-border text-secondary')
-        "
+        :class="cn('flex items-center gap-2 px-4 py-1 border-border text-secondary')"
+        @contextmenu.prevent="onContextMenu"
       >
-        <Icon :icon="tab.icon" width="24" height="24" />
+        <Icon
+          v-if="tab.value === modelValue"
+          :icon="tab.activeIcon || tab.icon || 'material-symbols:folder-open-outline'"
+          width="24"
+          height="24"
+        />
+        <Icon v-else :icon="tab.icon || 'material-symbols:folder-outline'" width="24" height="24" />
         {{ tab.label }}
       </TabsTrigger>
     </TabsList>
@@ -34,4 +58,11 @@ const modelValue = defineModel<string>('modelValue', { required: true })
       <slot :name="tab.value" />
     </TabsContent>
   </TabsRoot>
+  <ContextMenu
+    v-if="enableContextMenu && items?.length"
+    v-model="isOpen"
+    :virtual-element="virtualElement"
+    :popper="{ placement: popperPlacement }"
+    :items="items"
+  />
 </template>
