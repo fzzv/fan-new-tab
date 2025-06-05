57abbb528289bb57daf6304bccb336a96465cc16
fan <291950304@qq.com>
2025-06-05 19:57:21 +0800

    chore: 测试右击菜单功能

--- src/newtab/components/Bookmarks.vue
+++ src/newtab/components/Bookmarks.vue
@@ -1,30 +1,52 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { Icon } from '@iconify/vue'
 import { Tabs } from '@/components/tabs'
+import { useFavorite } from '@/composables/useFavorite.ts'
+import { openAddFavoriteDialog } from '@/composables/useDialog'
+import Button from '@/components/button/Button.vue'
+import type { MenuItemType } from '@/components/context-menu'
+import type { TabItem } from '@/components/tabs'
 
-const tabs = [
+const { favorites, removeFavorite } = useFavorite()
+
+const menuItems = [
   {
-    value: 'bookmarks',
-    label: 'Bookmarks',
-    icon: 'material-symbols:bookmarks-outline-rounded',
+    label: '编辑',
+    icon: 'material-symbols:edit-square-outline',
+    click: () => {
+      console.log(1)
+    },
   },
   {
-    value: 'bookmarks1',
-    label: '1b',
-    icon: 'material-symbols:bookmarks-outline-rounded',
+    label: '删除',
+    icon: 'material-symbols:delete-outline-rounded',
+    click: (item: TabItem) => {
+      if (item) {
+        removeFavorite(item)
+      }
+    },
   },
-]
+] as MenuItemType[]
 
-const modelValue = ref('bookmarks')
+const modelValue = ref('')
+
+onMounted(() => {
+  console.log(favorites.value)
+  modelValue.value = favorites.value[0].value
+})
 </script>
 
 <template>
-  <Tabs :tabs="tabs" v-model="modelValue">
-    <template #bookmarks>
-      {{ modelValue }}
-    </template>
-    <template #bookmarks1>
-      {{ modelValue }}
+  <Tabs :tabs="favorites" v-model="modelValue" :items="menuItems">
+    <template v-for="tab in favorites" :key="tab.value" #[tab.value]>
+      {{ tab.label }}
+      <Button
+        size="icon"
+        @click="openAddFavoriteDialog"
+      >
+        <Icon icon="material-symbols:add" width="24" height="24" />
+      </Button>
     </template>
   </Tabs>
 </template>
