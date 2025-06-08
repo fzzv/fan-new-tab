eea92da14875c35a237ecf58e0f01e2cf8c5afa0
fan <291950304@qq.com>
2025-06-09 00:39:13 +0800

    chore: 添加页面设置功能弹窗

--- src/composables/useDialog.ts
+++ src/composables/useDialog.ts
@@ -1,5 +1,6 @@
 import { ref } from 'vue'
 
+// 添加搜索引擎 弹窗
 export const isAddEngineDialog = ref(false)
 
 export const openAddEngineDialog = () => {
@@ -10,6 +11,7 @@ export const closeAddEngineDialog = () => {
   isAddEngineDialog.value = false
 }
 
+// 添加收藏夹 弹窗
 export const isAddFavoriteDialog = ref(false)
 
 export const openAddFavoriteDialog = () => {
@@ -18,4 +20,15 @@ export const openAddFavoriteDialog = () => {
 
 export const closeAddFavoriteDialog = () => {
   isAddFavoriteDialog.value = false
+}
+
+// 设置功能 弹窗
+export const isPageSettingDialog = ref(false)
+
+export const openPageSettingDialog = () => {
+  isPageSettingDialog.value = true
+}
+
+export const closePageSettingDialog = () => {
+  isPageSettingDialog.value = false
 }
