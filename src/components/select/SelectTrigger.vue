f696c999445f081f81d97afc670ddd3d5c054624
fan <291950304@qq.com>
2025-07-02 17:24:46 +0800

    fix: dark模式下的边框颜色问题

--- src/components/select/SelectTrigger.vue
+++ src/components/select/SelectTrigger.vue
@@ -21,7 +21,7 @@ withDefaults(
     :class="
       cn(
         'flex h-10 min-w-40 items-center shadow-md justify-between border-2 border-input',
-        'border-black bg-transparent px-4 py-2 ring-offset-background data-placeholder:text-muted-foreground',
+        'border-border bg-transparent px-4 py-2 ring-offset-background data-placeholder:text-muted-foreground',
         'focus:outline-hidden disabled:cursor-not-allowed disabled:opacity-50',
         $attrs.class as ClassValue,
       )
